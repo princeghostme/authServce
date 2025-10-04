c196edd87f2b61382fdca05dd98b132ebe194ca5
prince <coolkidzzz207@gmail.com>
2025-10-04 18:18:27 +0530

    first commit

---
+++ auth-servce/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace auth_servce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public IJwtService jwtservice = null;
+
+        public AuthController(IJwtService _jwtservice)
+        {
+            jwtservice = _jwtservice;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> userLogin([FromBody] UserLogin _usr)
+        {
+            var token = new TokenDetail() {
+                firstName = "John",
+                lastName = "Doe",
+            };
+            var _token = await jwtservice.encode(token);
+            return Ok(_token);
+        }
+
+        [HttpPost("decode")]
+        public async Task<IActionResult> decode([FromBody] string _usr)
+        {
+
+            var _token = await jwtservice.decode(_usr);
+            return Ok(_token);
+        }
+    }
+}
